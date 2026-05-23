7470d0a7e74c1396256de4b418c8b6539a04b326
rangeset <Rangeset24@outlook.com>
2026-05-23 15:46:39 +0800

    feat(window): change window to translucent

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    QWidget w;
+    w.setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    w.setAttribute(Qt::WA_TranslucentBackground);
+    w.resize(640, 480);
+    w.show();
     return QApplication::exec();
 }
